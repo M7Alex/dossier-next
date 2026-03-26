40453a8d0293a5b114ed3e96af28fbfb8f4ddf40
M7Alex <b7alex07@gmail.com>
2026-03-26 19:51:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,3 @@ const nextConfig = {
   reactStrictMode: false,
   experimental: { serverComponentsExternalPackages: ['@vercel/kv'] }
 }
-module.exports = nextConfig
-headers: async () => [
-     { source: '/(.*)', headers: [{ key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' }] }
-   ]
